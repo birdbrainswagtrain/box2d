6dbd15871a301e8577d47c084b82d018db96c3cf
Adam Coggeshall <dameerkat@gmail.com>
2020-04-08 14:31:28 -0600

    Add joint to build.

--- Box2D/Box2D/Dynamics/Joints/b2Joint.cpp
+++ Box2D/Box2D/Dynamics/Joints/b2Joint.cpp
@@ -32,7 +32,7 @@
 #include <Box2D/Dynamics/b2World.h>
 #include <Box2D/Common/b2BlockAllocator.h>
 
-#include <new>
+#include <memory.h>
 
 b2Joint* b2Joint::Create(const b2JointDef* def, b2BlockAllocator* allocator)
 {
